84536c10d26287858ae6d81fcfa701290e15c6c7
salomon.cjl <contrerassalomon6@gmail.com>
2023-12-07 17:29:21 -0300

    creacion del Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,22 +1,37 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
 
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
     // console.log(email, password, username);
-
+    
     try {
+        
+        const passwordHash = await bcrypt.hash(password, 10) // Nos da un string cualquiera
+        
         const newUser = new User({
             username,
             email,
-            password
+            password : passwordHash
         });
-
+        
         const userSaved = await newUser.save();
-        res.json(userSaved)
+        const token = await createAccessToken({ id: userSaved._id });
+        res.cookie('token', token);
+  
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            message: 'Usuario creado'
+        })
+      
         
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
     
 }
