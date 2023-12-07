c45e7728772345f201f333fe94b9151cecde042a
salomon.cjl <contrerassalomon6@gmail.com>
2023-12-07 02:39:50 -0300

    testeando controller register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,24 @@
+import User from "../models/user.model.js";
 
 
+export const register = async (req, res) => {
+    const {email, password, username} = req.body
+    // console.log(email, password, username);
 
-export const register = (req, res) => res.send('Register')
+    try {
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+
+        const userSaved = await newUser.save();
+        res.json(userSaved)
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
 
 export const login = (req, res) => res.send('Login')
