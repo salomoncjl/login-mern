fd31f2da44d71dabdbb6664f833961a50e1f17c3
salomon.cjl <contrerassalomon6@gmail.com>
2023-12-07 22:19:14 -0300

    Login creado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,4 +36,35 @@ export const register = async (req, res) => {
     
 }
 
-export const login = (req, res) => res.send('Login')
+export const login = async (req, res) => {
+    const {email, password} = req.body
+    
+    try {
+
+        const userFound = await User.findOne({email})
+        if (!userFound) return res.status(400).json({ messague: 'User not found'});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({ messague: 'Incorrect password' });
+        
+        const token = await createAccessToken({ id: userFound._id });
+
+        res.cookie('token', token);
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            message: 'Usuario logeado'
+        }) 
+    }   catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0),
+    });
+    return res.sendStatus(200);
+}
